42cb915e1f319d1450c6e5617d6797dfb80f838d
glyxbaer <chwolf91@gmail.com>
2013-03-05 15:24:42 +0100

    consistency test now works

--- src/com/beowulf/ScheduleValidator/ScheduleValidator.java
+++ src/com/beowulf/ScheduleValidator/ScheduleValidator.java
@@ -2,29 +2,33 @@ package com.beowulf.ScheduleValidator;
 
 import java.io.File;
 
+
 import com.beowulf.ScheduleValidator.model.*;
 import com.beowulf.ScheduleValidator.test.ConsistencyTest;
 import com.beowulf.ScheduleValidator.util.FileParser;
 
-public class ScheduleValidator {
+public class ScheduleValidator
+{
 
-	public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
-		FileParser myParser = new FileParser(
-				new File("conf\\constraints.conf"), new File(
-						"conf\\definitions.conf"));
+        FileParser myParser = new FileParser(new File("conf\\constraints.conf"), new File("conf\\definitions.conf"));
 
-		University myUni = myParser.parse();
-		myUni.print();
+        University myUni = myParser.parse();
+ 
+        
 
-		// ConsistencyTest myTest = new ConsistencyTest(myUni);
-		//
-		// if (myTest.getResult()) {
-		// System.out.println("Timetable is consistent");
-		// } else {
-		// System.out.println("Errors in timetable:\n" + myTest.getMsg());
-		// }
+        
+        if (ConsistencyTest.testDefinitions(myUni))
+        {
+            System.out.println("Timetable is consistent");
+        }
+        else
+        {
+            System.out.println("Timetable is NOT consistent, please check your definitions file on consistency");
+        }
 
-	}
+    }
 
 }
--- src/com/beowulf/ScheduleValidator/test/ConsistencyTest.java
+++ src/com/beowulf/ScheduleValidator/test/ConsistencyTest.java
@@ -1,71 +1,75 @@
 package com.beowulf.ScheduleValidator.test;
 
+
+import java.util.HashMap;
+
 import org.allen.temporalintervalrelationships.Constraint;
 import org.allen.temporalintervalrelationships.ConstraintNetwork;
 import org.allen.temporalintervalrelationships.Node;
 
 import com.beowulf.ScheduleValidator.model.*;
 
-public class ConsistencyTest {
+public class ConsistencyTest
+{
 
-	private boolean result;
-	private String msg;
+    private boolean result;
 
-	public ConsistencyTest(University uni) {
+    public ConsistencyTest(University uni)
+    {
 
-		test(uni);
+        result = testDefinitions(uni);
 
-	}
+    }
 
-	public boolean getResult() {
-		return result;
-	}
+    public boolean getResult()
+    {
+        return result;
+    }
 
-	public String getMsg() {
-		return msg;
-	}
 
-	private boolean test(University uni) {
+    static public boolean testDefinitions(University uni)
+    {
 
-		// TODO test everything for consistency here
-		result = false;
-		msg = "Alles falsch";
-		
-		ConstraintNetwork<String> myConstraintNetwork = new ConstraintNetwork<String>();
-		
-		for(Relation rel : uni.getRelations())
-		{
-		    Node<String> nodeA = new Node<String>(rel.getX1());
-		    myConstraintNetwork.addNode(nodeA);
-		    Node<String> nodeB = new Node<String>(rel.getX2());
-		    myConstraintNetwork.addNode(nodeB);
-		    
-		    
-		    Constraint<String> constraintAB = new Constraint<String> (nodeA,nodeB,ConstraintNetwork.bin_equals);
-		    
-		    
-		}
+        ConstraintNetwork<Lecture> myConstraintNetwork = new ConstraintNetwork<Lecture>();
+        HashMap<String, Node<Lecture>> hashedmap = new HashMap<String,Node<Lecture>>();
         
-        Node<String> nodeA = new Node<String>("A");
-        myConstraintNetwork.addNode(nodeA);
-        Node<String> nodeB = new Node<String>("B");
-        myConstraintNetwork.addNode(nodeB);
-        Node<String> nodeC = new Node<String>("C");
-        myConstraintNetwork.addNode(nodeC);
-        Node<String> nodeD = new Node<String>("D");
-        myConstraintNetwork.addNode(nodeD);
-        Constraint<String> constraintAB = new Constraint<String> (nodeA,nodeB,ConstraintNetwork.bin_equals);
-        myConstraintNetwork.addConstraint(constraintAB);
-        Constraint<String> constraintBC = new Constraint<String> (nodeB,nodeC,ConstraintNetwork.bin_equals);
-        myConstraintNetwork.addConstraint(constraintBC);
-        Constraint<String> constraintCD = new Constraint<String> (nodeC,nodeD,ConstraintNetwork.bin_equals);
-        myConstraintNetwork.addConstraint(constraintCD);
-        assertEquals(myConstraintNetwork.pathConsistency(),true);      
-        Constraint<String> constraintAD = new Constraint<String> (nodeA,nodeD,ConstraintNetwork.bin_overlaps);
-        myConstraintNetwork.addConstraint(constraintAD);
-        assertEquals(myConstraintNetwork.pathConsistency(),false);  
-		return result;
-		
-	}
+        
+        for (Relation rel : uni.getRelations())
+        {
+            Node<Lecture> nodeA;
+            Node<Lecture> nodeB;
+           
+            
+            if((nodeA = hashedmap.get(rel.getX1().getName())) == null)
+            {
+                
+                nodeA = new Node<Lecture>(rel.getX1());
+                hashedmap.put(rel.getX1().getName(), nodeA);
+                myConstraintNetwork.addNode(nodeA);
+            
+            }
+            
+            if((nodeB = hashedmap.get(rel.getX2().getName())) == null)
+            {
+                
+                nodeB = new Node<Lecture>(rel.getX2());
+                hashedmap.put(rel.getX2().getName(), nodeB);
+                myConstraintNetwork.addNode(nodeB);
+            }
+            
+            
+            Short vektor = 0;
+            for (Short allen : rel.getCons())
+            {
+                vektor = (short) (vektor | allen);
+                
+            }
+            myConstraintNetwork.addConstraint(new Constraint<Lecture>(nodeA, nodeB, vektor));
+
+        }
+
+        return myConstraintNetwork.pathConsistency();
+
+    }
 
 }
