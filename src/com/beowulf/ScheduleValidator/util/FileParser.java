9625ab2c303fe4f75f8550e9f46fd51f8c141d85
JREB <jreb1990@gmail.com>
2013-03-05 10:44:51 +0100

    Parser enhancement

--- src/com/beowulf/ScheduleValidator/util/FileParser.java
+++ src/com/beowulf/ScheduleValidator/util/FileParser.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+import org.allen.temporalintervalrelationships.ConstraintNetwork;
+
 import com.beowulf.ScheduleValidator.model.*;
 
 public class FileParser {
@@ -40,14 +42,49 @@ public class FileParser {
 					uni.addLecture(data[0], lec);
 				}
 				br.close();
-				
+
 				// Read the constraintFile and add the lines as objects to uni
 				br = new BufferedReader(new FileReader(constraintFile));
 				while ((line = br.readLine()) != null) {
 					String[] data = line.split(";");
 					Lecture l1 = uni.getlectures().get(data[0]);
 					Lecture l2 = uni.getlectures().get(data[1]);
-					String[] rels = data[2].split(",");
+					String[] cons = data[2].split(",");
+					Relation rel = new Relation(l1, l2);
+					for (int x = 0; x < cons.length; x++) {
+						switch (cons[x]) {
+						case "=":
+							rel.addConstraint(ConstraintNetwork.bin_equals);
+						case "<":
+							rel.addConstraint(ConstraintNetwork.bin_before);
+						case ">":
+							rel.addConstraint(ConstraintNetwork.bin_after);
+						case "d":
+							rel.addConstraint(ConstraintNetwork.bin_during);
+						case "di":
+							rel.addConstraint(ConstraintNetwork.bin_contains);
+						case "o":
+							rel.addConstraint(ConstraintNetwork.bin_overlaps);
+						case "oi":
+							rel.addConstraint(ConstraintNetwork.bin_overlappedby);
+						case "m":
+							rel.addConstraint(ConstraintNetwork.bin_meets);
+						case "mi":
+							rel.addConstraint(ConstraintNetwork.bin_metby);
+						case "s":
+							rel.addConstraint(ConstraintNetwork.bin_starts);
+						case "si":
+							rel.addConstraint(ConstraintNetwork.bin_startedby);
+						case "f":
+							rel.addConstraint(ConstraintNetwork.bin_finishes);
+						case "fi":
+							rel.addConstraint(ConstraintNetwork.bin_finishedby);
+						}
+
+						rel.addConstraint(cons[x]);
+					}
+
+					uni.addRelation(rel);
 				}
 				br.close();
 
@@ -59,7 +96,6 @@ public class FileParser {
 			// Use Case 2: planFile
 
 		}
-
 
 		// TODO fill uni
 
